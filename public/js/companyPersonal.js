b55f52152540430836d6aaa8fa66ac75736a4964
徐潞潞 <lulu@siaedu.net>
2016-05-31 12:43:46 +0800

    细节调整

--- public/js/companyPersonal.js
+++ public/js/companyPersonal.js
@@ -6,4 +6,20 @@ $(function(){
 		else {
 			$('.sum').text(0);
 		}
+	var flag_login = true;
+    $(".arrow-top").click(function(){
+      if(flag_login){
+            $('.TABLE-PANEL').removeClass('active');
+            $('.APP-SEARCH li').removeClass('active');
+            $('.APP-SEARCH').css({height:'auto'})
+          $('.arrow-top').addClass('PortraitImg');
+          $(".MainLogin").css({height:40});
+          $(".MainNav").css({height:0});
+          flag_login = false;
+        }else{
+          $('.arrow-top').removeClass('PortraitImg');
+          $(".MainLogin").css({height:0});
+          flag_login = true
+        }
+    });
 })
